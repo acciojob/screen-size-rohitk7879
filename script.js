6ee64263b7d124b4594dd7d5cab7559fb115b914
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-22 00:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+
+width.innerHTML = "Width: "  + window.innerWidth;
+height.innerHTML = "Height: " + window.innerHeight;
+window.addEventListener('resize', ()=>{
+	width.innerHTML = "Width: " + window.innerWidth;
+height.innerHTML = "Height: " + window.innerHeight;
+})
